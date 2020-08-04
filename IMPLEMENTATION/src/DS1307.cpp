e9971c118486a6d87d02562a1b2f8be89d82306a
MCLEANS <jackagwaya@gmail.com>
2020-08-04 15:50:05 +0300

    created RTC driver files

---
+++ IMPLEMENTATION/src/DS1307.cpp
@@ -0,0 +1,21 @@
+/*
+ * DS1307.cpp
+ *
+ *  Created on: Aug 4, 2020
+ *      Author: MCLEANS
+ */
+
+#include "DS1307.h"
+
+namespace custom_libraries {
+
+DS1307::DS1307() {
+	// TODO Auto-generated constructor stub
+
+}
+
+DS1307::~DS1307() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace custom_libraries */
---
+++ IMPLEMENTATION/src/DS1307.h
@@ -0,0 +1,21 @@
+/*
+ * DS1307.h
+ *
+ *  Created on: Aug 4, 2020
+ *      Author: MCLEANS
+ */
+
+#ifndef DS1307_H_
+#define DS1307_H_
+
+namespace custom_libraries {
+
+class DS1307 {
+public:
+	DS1307();
+	virtual ~DS1307();
+};
+
+} /* namespace custom_libraries */
+
+#endif /* DS1307_H_ */
