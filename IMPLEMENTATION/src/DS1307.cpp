2a818a85bf95b368a10413e20ae8e4861e88cadd
MCLEANS <jackagwaya@gmail.com>
2020-08-04 16:14:58 +0300

    fixed buffer issue

--- IMPLEMENTATION/src/DS1307.cpp
+++ IMPLEMENTATION/src/DS1307.cpp
@@ -32,15 +32,15 @@ void DS1307::set_time(RTC_Time rtc_time){
 	set_buffer[5] = decimal_to_BCD(rtc_time.month);
 	set_buffer[6] = decimal_to_BCD(rtc_time.year);
 
-	i2c.write_bytes(RTC_ADDRESS,0x00,set_buffer,BUFFER_SIZE);
+	write_bytes(RTC_ADDRESS,0x00,set_buffer,BUFFER_SIZE);
 }
 
 RTC_Time DS1307::get_time(){
 	RTC_Time rtc_time;
 
-	i2c.write_memp(RTC_ADDRESS,0x00);
-	i2c.read_bytes(RTC_ADDRESS,buffer,BUFFER_SIZE);
-	rtc_time.second =i2c.BCD_to_decimal(buffer[0]);
+	write_memp(RTC_ADDRESS,0x00);
+	read_bytes(RTC_ADDRESS,buffer,BUFFER_SIZE);
+	rtc_time.second = BCD_to_decimal(buffer[0]);
 	rtc_time.minute = BCD_to_decimal(buffer[1]);
 	rtc_time.hour = BCD_to_decimal(buffer[2]);
 	rtc_time.day = BCD_to_decimal(buffer[3]);
